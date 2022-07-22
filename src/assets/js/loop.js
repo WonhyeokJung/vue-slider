e3111da7e915d7d73bff85a3d6afff4ddb049d53
WonhyeokJung <wonhyeok.contact@gmail.com>
2022-07-22 10:52:56 +0900

    [fix]일부 코드 수정

--- src/assets/js/loop.js
+++ src/assets/js/loop.js
@@ -1,5 +1,10 @@
-function createLoop () {
-  return 0
+function createLoop (slides) {
+  if (slides === undefined) return
+  console.log(slides[0].children)
+  const newSlides = {
+    children: slides[0].children
+  }
+  console.log(newSlides)
 }
 
 export { createLoop }
